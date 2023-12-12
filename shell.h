325af9c07c71e12b01e4ee8e86f930c82008facd
yusuf-muibi <muibiyusuf@hotmail.com>
2023-12-12 02:53:15 -0800

    main

--- shell.h
+++ shell.h
@@ -100,7 +100,7 @@ int readfd;
 int history_count;
 } info_t;
 
-#define INFO_INIT {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, \
+#define SHELL_INFO_INIT {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, \
 NULL, NULL, 0, 0, NULL, 0, 0, 0}
 
 /**
